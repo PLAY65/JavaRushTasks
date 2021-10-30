7105797465e049ea77d6ec41c9939a1071ecb969
Play65 <kardash.sergey@gmail.com>
2021-10-30 23:35:27 +0300

    com.javarush.task.task23.task2312 Snake

--- 3.JavaMultithreading/src/com/javarush/task/task23/task2312/Snake.java
+++ 3.JavaMultithreading/src/com/javarush/task/task23/task2312/Snake.java
@@ -58,9 +58,23 @@ public class Snake {
 
     };
 
-    void move(int a, int b){
+    void move(int dx, int dy){
+        SnakeSection head = new SnakeSection(sections.get(0).getX() + dx, sections.get(0).getY() + dy);
 
-    };
+        checkBorders(head);
+        if (!isAlive) return;
+        checkBody(head);
+        if (!isAlive) return;
+
+        Mouse mouse = Room.game.getMouse();
+        if (head.getX() == mouse.getX() && head.getY() == mouse.getY()){
+            sections.add(0, new SnakeSection(getX()+dx, getY()+dy));
+            Room.game.eatMouse();
+        }else{
+            sections.add(0, new SnakeSection(getX()+dx, getY()+dy));
+            sections.remove(sections.size()-1);
+        }
+    }
 
     public void checkBorders(SnakeSection head){
         if ((head.getX() < 0 || head.getX() >= Room.game.getWidth()) || head.getY() < 0 || head.getY() >= Room.game.getHeight()) {
