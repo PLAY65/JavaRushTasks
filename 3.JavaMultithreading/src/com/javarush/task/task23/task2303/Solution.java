cc71cb6861687d45a62db6156dd58ab3dba875a6
Play65 <kardash.sergey@gmail.com>
2021-11-06 12:00:02 +0300

    com.javarush.task.task23.task2303

--- 3.JavaMultithreading/src/com/javarush/task/task23/task2303/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task23/task2303/Solution.java
@@ -2,6 +2,14 @@ package com.javarush.task.task23.task2303;
 
 /* 
 Запрети создание экземпляров класса
+Запрети создание экземпляров класса Listener.
+
+
+Requirements:
+1. Класс Listener должен быть создан внутри класса Solution.
+2. Класс Listener должен быть публичным.
+3. Класс Listener должен быть статическим.
+4. Должна быть запрещена возможность создавать экземпляры класса Listener.
 */
 
 public class Solution {
